d7b4417bdbfd08d71a38385bf77f1e8132421be9
Sajal Gupta <guptasajal6@gmail.com>
2023-10-14 18:17:10 +0530

    removed formidable 2

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -21,7 +21,6 @@ router.post('/blogs-categories-tags', listAllBlogsCategoriesTags);
 router.post('/blogsByCategory', blogsByCategory);
 router.get('/blog/:slug', read);
 router.delete('/blog/:slug',remove);
-router.put('/blog/:slug',  update); 
 router.get('/blog/photo/:slug', photo);
 router.post('/blogs/related', listRelated);
 router.get('/blogs/search', listSearch);
@@ -29,6 +28,5 @@ router.get('/blogs/search', listSearch);
 // auth user blog crud
 router.post('/user/blog', create);
 router.delete('/user/blog/:slug' , remove);
-router.put('/user/blog/:slug' , update);
 
 module.exports = router;
